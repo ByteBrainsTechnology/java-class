9c9b07c1fe6be3fa14908d04684cfaa6bcc59e76
bytebrainstechnology <bytebrainstechnology@gmail.com>
2023-03-02 22:38:39 +0530

    first commit

---
+++ string-examples/src/com/bytebrains/StringExample.java
@@ -0,0 +1,12 @@
+package com.bytebrains;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String s1 = "abc";
+		String s2 = new String("abc");
+		String s3 = "abc";
+		String s4 = new String("abc");
+		String s5 = new String("abc").intern();
+		
+	}
+}
